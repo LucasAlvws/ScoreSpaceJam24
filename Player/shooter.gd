7ce51c535a6ddd10e71fd4e468ad5ba0567d53ca
Maieron <gbmaieron@gmail.com>
2023-01-21 15:18:06 -0300

    Balas

---
+++ Player/shooter.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+const bullet_scene = preload("res://Player/bala.tscn")
+
+const angulo = 270.0
+const spawn_point_count = 5
+const radius = 1
+
+func _ready():	
+	var step = 2 * PI / spawn_point_count
+	
+	for i in range(spawn_point_count):
+		var spawn_point = Node2D.new()
+		var pos = Vector2(radius, 0).rotated(step * i)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		self.add_child(spawn_point)
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("up", true):
+		shoot()
+
+func shoot():
+	for s in self.get_children():
+		var bullet = bullet_scene.instance()
+		get_tree().root.add_child(bullet)
+		bullet.position = s.global_position
+		bullet.rotation = s.global_rotation	
+		
+
