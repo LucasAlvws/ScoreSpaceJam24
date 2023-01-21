125f2a29ea1c40620ab5dc1236d6802d8824d374
Lucas Alves <108438456+LucasAlvws@users.noreply.github.com>
2023-01-20 23:42:50 -0300

    colisao dinamica com  forma geometrica

--- boneco.cs
+++ boneco.cs
@@ -3,18 +3,20 @@ using System;
 
 public class boneco : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+    
     private Vector2 velocity = new Vector2();
     private int speed = 300;
- // Called every frame. 'delta' is the elapsed time since the previous frame.
+    private double n, n2 = 4, x, y;
+    private int i;
+    private Godot.CollisionPolygon2D col;
+    public override void _Ready()
+    {
+        _Draw();
+    
+        
+        
+    }
+    
     public void GetInput()
     {
         velocity = new Vector2();
@@ -38,5 +40,42 @@ public class boneco : KinematicBody2D
     {
         GetInput();
         velocity = MoveAndSlide(velocity);
+        setar_colisao();
+        
     }
+
+    private Vector2[] devolve_vetor(double n2)
+    {
+        var points = new System.Collections.Generic.List<Vector2>();
+        n = 360/n2;
+        n = Mathf.Deg2Rad((float)n);
+        for(i=0; i<n2;i++){
+            x =  Math.Sin(n*i);
+            y = (Math.Cos(n*i))*-1;
+            points.Add(new Vector2((float)x,(float)y));
+        }
+        return points.ToArray();
+    }
+
+    private Color[] devolve_color(double n2)
+    {
+        var color = new System.Collections.Generic.List<Color>();
+        n = 360/n2;
+        n = Mathf.Deg2Rad((float)n);
+        for(i=0; i<n2;i++){
+            x =  Math.Sin(n*i);
+            y = (Math.Cos(n*i))*-1;
+            color.Add(new Color(0,0,0));
+        }
+        return color.ToArray();
+    }
+    
+    public override void _Draw()
+    {
+        DrawPolygon(devolve_vetor(n2), devolve_color(n2));
+    }
+    private void setar_colisao(){
+        col = GetNode<Godot.CollisionPolygon2D>("colisao");
+        col.Polygon = devolve_vetor(n2);
+    }    
 }
